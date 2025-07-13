bae7dea7fc7e287de9a5ccc79470115be369034f
sajood ali <sajoodali486@gmail.com>
2025-07-14 02:09:40 +0500

    db connect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express"; 
+const app = express();
+
+
+// timeline 20:00 min 
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+
+    } catch (error) {
+        console.error('ERROR: ',error)
+        throw error
+    }
+})()
