066ec53763119fe8817ff7aac6d8d6f5640c58ec
sajood ali <sajoodali486@gmail.com>
2025-07-25 18:32:31 +0500

    db conect

--- src/index.js
+++ src/index.js
@@ -9,23 +9,23 @@ connectDB()
 
 // timeline 30min
 
-// import express from "express";
-// const app = express();
+import express from "express";
+const app = express();
 
 
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-//     app.on("ERROr", (error) => {
-//       console.log("ERROR: ", error);
-//       throw error;
-//     });
+;(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("ERROr", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
 
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listening on Port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("ERROR: ", error);
-//     throw error;
-//   }
-// })();
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on Port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw error;
+  }
+})();
