5c9208926d0ecdb9402b769ffa9a485efc7986ac
sajood ali <sajoodali486@gmail.com>
2025-12-29 19:36:21 +0500

    auth fnction create

---
+++ src/controlor/auth.js
@@ -0,0 +1,15 @@
+export const login = (req, res) => {
+    res.send("login");
+};
+
+export const register = (req, res) => {
+    res.send("register");
+};
+
+export const logout = (req, res) => {
+    res.send("logout");
+};
+
+export const porfile = (req, res) => {
+    res.send("profile");
+};
