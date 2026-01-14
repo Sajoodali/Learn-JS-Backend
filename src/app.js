c6d7a9998ad2d8ae971fcd55ed03ec21a309b354
sajood ali <sajoodali486@gmail.com>
2026-01-14 23:15:35 +0500

    some chnages

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
