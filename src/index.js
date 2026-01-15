1294e754b600496d1afdf7e96034296fbd6d7d04
sajood ali <sajoodali486@gmail.com>
2026-01-15 23:04:35 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -14,4 +14,8 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
+
 const PORT = process.env.PORT || 3000;
