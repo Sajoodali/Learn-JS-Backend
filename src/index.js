a12d0b3f5a5b210317ead74e77d6214842efe163
sajood ali <sajoodali486@gmail.com>
2026-01-18 04:55:57 +0500

    cllean code

--- src/index.js
+++ src/index.js
@@ -5,29 +5,13 @@ import connectDB  from "./db/db.js";
 dotenv.config();
 connectDB()
 
-// timeline 30min
 
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-app.get("/health", (req, res) => {
-    res.status(200).send("OK");
-});
-app.get("/health", (req, res) => {
-    res.status(200).send("OK");
-});
-
-app.get("/health", (req, res) => {
-    res.status(200).send("OK");
-});
-
-app.post("/data", express.json(), (req, res) => {
-    const receivedData = req.body;
-    res.status(201).json({ message: "Data received", data: receivedData });
-});
 
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
