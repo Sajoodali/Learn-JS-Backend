e8f02ac9512b302dd77de228c53515261e209f2d
sajood ali <sajoodali486@gmail.com>
2026-01-17 23:28:14 +0500

    commit

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
     res.status(200).send("OK");
 });
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
 
 app.post("/data", express.json(), (req, res) => {
     const receivedData = req.body;
