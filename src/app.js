bac6adbd1a713748fff5dce592dfaa206626d5dc
sajood ali <sajoodali486@gmail.com>
2026-01-22 23:46:11 +0500

    some changeing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-import mongooes from "mongoose";
+import mongooes,{Schema} from "mongoose";
 
 
 mongooes.connect("mongodb://localhost:27017/mydatabase", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}); 
+});      
 const db = mongooes.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
+
+
+console.log("Database connected successfully");
