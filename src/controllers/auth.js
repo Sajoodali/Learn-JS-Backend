cfb9f3a2b5e5ce913ab56b4c4c84b4a73eb5ee16
sajood ali <sajoodali486@gmail.com>
2026-01-26 23:16:00 +0500

    profile function

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -40,7 +40,16 @@ export const logout = (req, res) => {
 };
 
 export const porfile = (req, res) => {
-    res.send("profile");
+    const { userId } = req.params;
+    console.log("profile", userId);
+    try {
+        if (!userId) {
+            return res.status(400).json({ message: "User ID is required" });
+        }
+        res.status(200).json({ message: "User profile fetched successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
 };
 
 export const update = (req, res) => {
