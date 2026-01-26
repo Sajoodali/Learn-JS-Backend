679e4064b0b3784120a5af509a13f43618f7add8
sajood ali <sajoodali486@gmail.com>
2026-01-26 23:12:34 +0500

    auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,7 +3,17 @@ export const login = (req, res) => {
 }
 
 export const register = (req, res) => {
-    res.send("register");
+    const user ={ username, email, password, hobbies, usertype } = req.body;
+    console.log("register", username, email, password, hobbies, usertype);
+    try {
+        if(!username || !email || !password){
+            return res.status(400).json({ message: "All fields are required" });
+        }   
+        res.status(201).json({ message: "User registered successfully" });
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
 };
 
 export const logout = (req, res) => {
