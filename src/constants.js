0a661ae51c562336eea64506b85633000e16dce7
sajood ali <sajoodali486@gmail.com>
2026-02-01 22:44:58 +0500

    some changinng

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,15 @@
 export const DB_NAME = "DataBase"
+
+
+
+export const login = (req, res) => { {
+    const { email, password } = req.body;
+    console.log("login", email, password);
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required" });
+        }
+        res.status(200).json({ message: "User logged in successfully" });   
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
